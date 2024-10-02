74f014824934288f3fb196cbf1d4e9f255faa7f0
Alexander Soultanian <So5453Al1228@pusd.us>
2024-10-02 10:41:26 -0700

    done for sprintA

--- area_2d.gd
+++ area_2d.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_body_entered(body):
+	Node2D.score+=1
+	
